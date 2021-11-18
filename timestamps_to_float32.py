e5367c833199d85198c63d61afd68dfa66924453
danielmk <danielmuellermsc@gmail.com>
2021-11-18 13:22:37 +0100

    Bugfix

--- timestamps_to_float32.py
+++ timestamps_to_float32.py
@@ -3,6 +3,7 @@
 Convert time stamps to float32 dtype.
 """
 import shelve, os, fnmatch, sys
+import numpy as np
 
 in_path = sys.argv[1]
 out_path = sys.argv[2]
@@ -13,11 +14,10 @@ for root, dirnames, filenames in os.walk(in_path):
         os.makedirs(out_path, exist_ok=True)
 
         out_shelve_path = os.path.join(out_path, filename)
-        curr_file_path = os.path.join(root, filename)
-        assert os.path.isfile(curr_file_path)
-        print(curr_file_path)
+        # 
+        curr_file_path = os.path.join(root, '.'.join(filename.split('.')[:-1]))
+        # assert os.path.isfile(curr_file_path)
         with shelve.open(curr_file_path) as sh:
-
             print(list(sh.keys()))
             new_shelve = {'grid_spikes': 
                           {traj: {ps:[] for ps in sh['grid_spikes'][traj].keys()}
@@ -33,8 +33,6 @@ for root, dirnames, filenames in os.walk(in_path):
                             new_shelve[ct][traj][ps].append(sh[ct][traj][ps][idx].astype(np.float32))
                             
             with shelve.open(out_shelve_path) as new_file:
-                new_file = shelve.open(out_shelve_path)
-                
                 new_file['grid_spikes'] = new_shelve['grid_spikes']
                 new_file['granule_spikes'] = new_shelve['granule_spikes']
                 new_file['parameters'] = sh['parameters']
