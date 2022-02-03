06b129f0b1a87dc2e5ce6b9a4738176c047af089
danielmk <danielmuellermsc@gmail.com>
2022-02-03 16:36:12 +0100

    400 GCs

--- tempotron_script.py
+++ tempotron_script.py
@@ -25,7 +25,6 @@ pr.add_argument('-grid_seed',
 pr.add_argument('-shuffling',
                 type=str,
                 help='Process shuffled or non-shuffled',
-                default='shuffled',
                 default='non-shuffled',
                 dest='shuffling')
 
@@ -34,14 +33,14 @@ grid_seed = args.grid_seed
 shuffling = args.shuffling
 
 """Parameters"""
-seed = 63
+seed = 89
 np.random.seed(seed)
 epochs = 200  # Number of learning epochs
 total_time = 2000.0 # Simulation time
 V_rest = 0.0  # Resting potential
 learning_rate = 1e-3
-n_cells = 200
-threshold = 5
+n_cells = 400
+threshold = 8
 tau = 10.0
 tau_s = tau / 4.0
 # efficacies = 1.8 * np.random.random(n_cells) - 0.50
@@ -50,61 +49,59 @@ trajectory_1 = '75'
 trajectory_2 = '60'
 
 cell_type = 'granule_spikes'
-for x in range(1:11):
-    """Load data"""
-    dirname = os.path.dirname(__file__)
-    example_data = os.path.join(
-        dirname, 'data', 'tempotron',
-        f'grid-seed_duration_shuffling_tuning_{x}_2000_{shuffling}_full')
-    data = shelve.open(example_data)
-    
-    """Structure and label spike times"""
-    spike_times1 = [(np.array(data[trajectory_1][cell_type][x][:n_cells], dtype=object), True) for x in data[trajectory_1][cell_type]]
-    spike_times2 = [(np.array(data[trajectory_2][cell_type][x][:n_cells], dtype=object), False) for x in data[trajectory_2][cell_type]]
-    all_spikes = np.array(spike_times1 + spike_times2, dtype=object)
-    
-    # Initialize synaptic efficiencies
-    efficacies = np.random.rand(n_cells)
-    print('synaptic efficacies:', efficacies, '\n')
-    
-    tempotron = Tempotron(V_rest, tau, tau_s, efficacies,total_time, threshold, jit_mode=True, verbose=True)
-    pre_accuracy = tempotron.accuracy(all_spikes)
-    print("Pre-training accuracy: " + 
-          str(pre_accuracy))
-    tempotron.plot_membrane_potential(0, total_time, all_spikes[0][0])
-    # sys.exit()
-    tempotron.train(all_spikes, epochs, learning_rate=learning_rate)
-    print(tempotron.efficacies)
-    trained_accuracy = tempotron.accuracy(all_spikes)
-    print("Post-training accuracy: " + 
-          str(trained_accuracy))
-    tempotron.plot_membrane_potential(0, total_time, all_spikes[0][0])
-    plt.show()
-    
-    """Save results to database"""
-    grid_seed, duration, shuffling, network = example_data.split(os.sep)[-1].split("_")[-4:]
-    
-    
-    db_path = os.path.join(
-        dirname, 'data', 'tempotron.db')
-    con = sqlite3.connect(db_path)
-    cur = con.cursor()
-    cur.execute(f"""INSERT INTO tempotron_run VALUES 
-                ({seed}, {epochs},{total_time},{V_rest},{tau},{tau_s},{threshold},
-                 {learning_rate},{n_cells},{trajectory_1},{trajectory_2},{pre_accuracy},
-                 {trained_accuracy},{grid_seed}, {duration}, 
-                 '{shuffling}', '{network}', '{cell_type}')
-                """)
-    
-    con.commit()
-    con.close()
-    
-    
-    # con = sqlite3.connect(db_path)
-    # cur = con.cursor()
-    # rows = cur.execute(f"""SELECT * FROM tempotron_run""")
-    """         
-    for x in rows:
-        print(x)
-                
-    """
+"""Load data"""
+dirname = os.path.dirname(__file__)
+example_data = os.path.join(
+    dirname, 'data', 'tempotron',
+    f'grid-seed_duration_shuffling_tuning_{x}_2000_{shuffling}_full')
+data = shelve.open(example_data)
+
+"""Structure and label spike times"""
+spike_times1 = [(np.array(data[trajectory_1][cell_type][x][:n_cells], dtype=object), True) for x in data[trajectory_1][cell_type]]
+spike_times2 = [(np.array(data[trajectory_2][cell_type][x][:n_cells], dtype=object), False) for x in data[trajectory_2][cell_type]]
+all_spikes = np.array(spike_times1 + spike_times2, dtype=object)
+
+# Initialize synaptic efficiencies
+efficacies = np.random.rand(n_cells)
+print('synaptic efficacies:', efficacies, '\n')
+
+tempotron = Tempotron(V_rest, tau, tau_s, efficacies,total_time, threshold, jit_mode=True, verbose=True)
+pre_accuracy = tempotron.accuracy(all_spikes)
+print("Pre-training accuracy: " + 
+      str(pre_accuracy))
+tempotron.plot_membrane_potential(0, total_time, all_spikes[0][0])
+sys.exit()
+tempotron.train(all_spikes, epochs, learning_rate=learning_rate)
+print(tempotron.efficacies)
+trained_accuracy = tempotron.accuracy(all_spikes)
+print("Post-training accuracy: " + 
+      str(trained_accuracy))
+tempotron.plot_membrane_potential(0, total_time, all_spikes[0][0])
+plt.show()
+
+"""Save results to database"""
+grid_seed, duration, shuffling, network = example_data.split(os.sep)[-1].split("_")[-4:]
+
+db_path = os.path.join(
+    dirname, 'data', 'tempotron.db')
+con = sqlite3.connect(db_path)
+cur = con.cursor()
+cur.execute(f"""INSERT INTO tempotron_run VALUES 
+            ({seed}, {epochs},{total_time},{V_rest},{tau},{tau_s},{threshold},
+             {learning_rate},{n_cells},{trajectory_1},{trajectory_2},{pre_accuracy},
+             {trained_accuracy},{grid_seed}, {duration}, 
+             '{shuffling}', '{network}', '{cell_type}')
+            """)
+
+con.commit()
+con.close()
+
+
+# con = sqlite3.connect(db_path)
+# cur = con.cursor()
+# rows = cur.execute(f"""SELECT * FROM tempotron_run""")
+"""         
+for x in rows:
+    print(x)
+            
+"""
