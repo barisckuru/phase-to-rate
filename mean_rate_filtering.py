1294424d7366df837a12bbbe83b77015a3019a46
danielmk <danielmuellermsc@gmail.com>
2022-03-21 15:41:03 +0100

    Calculate mean_rate for each condition

---
+++ mean_rate_filtering.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 21 13:20:49 2022
+
+@author: Daniel
+"""
+import numpy as np
+import os
+import shelve
+import pandas as pd
+
+
+dirname = os.path.dirname(__file__)
+path = os.path.join(
+    dirname, 'data', 'tempotron')
+files = [x for x in os.listdir(path) if 'adjusted' in x]
+
+grid_seed = []
+shuffling = []
+network = []
+mean_rate = []
+for f in files:
+    f = f.split('.')[0]
+    curr_path = os.path.join(path, f)
+    curr_data = shelve.open(curr_path)
+    averages = []
+    for k1 in curr_data:
+        for k2 in curr_data[k1]['granule_spikes']:
+            n_s = [len(x) / 2 for x in curr_data[k1]['granule_spikes'][k2]]
+            curr_avg = np.array(n_s).mean()
+            averages.append(curr_avg)
+    file_average = np.array(averages).mean()
+    f_split = f.split('_')
+    grid_seed.append(f_split[4])
+    shuffling.append(f_split[6])
+    network.append(f_split[7])
+    mean_rate.append(file_average)
+
+d = {'grid_seed': grid_seed,
+     'shuffling': shuffling,
+     'network': network,
+     'mean_rate': mean_rate}
+df = pd.DataFrame(data=d)
+
+out_path = os.path.join(dirname, 'data', 'mean_rates.csv')
+df.to_csv(out_path)
